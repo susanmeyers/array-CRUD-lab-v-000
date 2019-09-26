78a1d6ff3da6337cb3e01dcd7cdae02c5ccd2009
Susan Meyers <smmeyers24@gmail.com>
2019-09-26 02:38:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -30,9 +30,9 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array.first(array)
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  array.last(array)
+  array.last
 end
