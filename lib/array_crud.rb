14bbe4fe3474c298d037c739411f6f8e4fb4f44a
Susan Meyers <smmeyers24@gmail.com>
2019-09-26 02:39:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -25,8 +25,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array.index(index_number)
-   return array[]
+  array(index_number).index
+   
 end
 
 def retrieve_first_element_from_array(array)
